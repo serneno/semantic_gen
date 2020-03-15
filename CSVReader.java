8bf9e64a7e6242c21963722360445a65bb114f81
serneno <haydnphk@gmail.com>
2020-03-15 14:13:10 -0700

    retrieves list of attributes and list of data

--- CSVReader.java
+++ CSVReader.java
@@ -1,15 +1,45 @@
+package semantic_gen;
+
+import java.util.ArrayList;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVReader {
-    BufferedReader bReader;
+    String fileName;                    // file name
+    private String[] attributeNames;    // attribute names
+    private ArrayList<String[]> data;   // stores individual data
     
-
     public CSVReader(String file) {
-        bReader = new BufferedReader(new FileReader(file));
+        fileName = file;
+        attributeNames = null;
+        data = new ArrayList<String[]>();
     }
 
+    public void csvParse() {
+        String currLine;    // current line in file
+        try {
+            BufferedReader bReader = new BufferedReader(new FileReader(fileName));
+            // Attribute Names
+            attributeNames = bReader.readLine().split(",");
 
+            // Stores each line in ArrayList data
+            while((currLine = bReader.readLine()) != null) {
+                String[] attributes = currLine.split(",");
+                data.add(attributes);
+            }
+            bReader.close();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String[] getAttributes() {
+        return attributeNames;
+    }
+
+    public ArrayList<String[]> getData() {
+        return data;
+    }
 }
