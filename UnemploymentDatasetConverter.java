c80f3bb9e392de8bfcc227fa33302c7061b6cece
serneno <haydnphk@gmail.com>
2020-03-22 15:36:12 -0700

    fixed unemployment

--- UnemploymentDatasetConverter.java
+++ UnemploymentDatasetConverter.java
@@ -54,27 +54,17 @@ public class UnemploymentDatasetConverter extends DatasetConverter{
 			Property date = model.createProperty(URI + "Date");
 			Property year = model.createProperty(URI + "Year");
 			Property month = model.createProperty(URI + "Month");
-			Property age16to19 = model.createProperty(URI + "SixteenToNineteen");
-			Property age20to24 = model.createProperty(URI + "TwentyToTwentyFour");
-			Property age25to34 = model.createProperty(URI + "TwentyFivetoThirtyFour");
-			Property age35to44 = model.createProperty(URI + "ThirtyFiveToFortyFour");
-			Property age45to54 = model.createProperty(URI + "FortyFiveToFiftyFour");
-			Property age55to64 = model.createProperty(URI + "FiftyFivetoSixtyFour");
-			Property age65Plus = model.createProperty(URI + "SixtyFivePlus");
-			String areaTypeValue = getData().get(i)[0];
+			Property seasonalAdj = model.createProperty(URI + "SeasonAdjustment");
+			Property nonSeasonalAdj = model.createProperty(URI + "NonSeasonalAdjustment");
 
+			String areaTypeValue = getData().get(i)[0];
 			String areaNameValue = getData().get(i)[1];
 			String dateValue = getData().get(i)[2];
 			String yearValue = getData().get(i)[3];
-
 			String monthValue = getData().get(i)[4];
-			String age16to19Value = getData().get(i)[5];
-			String age20to24Value = getData().get(i)[6];
-			String age25to34Value = getData().get(i)[7];
-			String age35to44Value = getData().get(i)[8];
-			String age45to54Value = getData().get(i)[9];
-			String age55to64Value = getData().get(i)[10];
-			String age65PlusValue = getData().get(i)[11];
+			String seasonalAdjValue = getData().get(i)[5];
+			String nonSeasonalAdjValue = getData().get(i)[6];
+
 			// placeholder till i figure out what the correct resource should be
 			Resource timeline = model.createResource("https://data.edd.ca.gov" + i);
 			timeline.addProperty(areaType, areaTypeValue);
@@ -82,13 +72,8 @@ public class UnemploymentDatasetConverter extends DatasetConverter{
 			timeline.addProperty(date, dateValue);
 			timeline.addProperty(year, yearValue);
 			timeline.addProperty(month, monthValue);
-			timeline.addProperty(age16to19, age16to19Value);
-			timeline.addProperty(age20to24, age20to24Value);
-			timeline.addProperty(age25to34, age25to34Value);
-			timeline.addProperty(age35to44, age35to44Value);
-			timeline.addProperty(age45to54, age45to54Value);
-			timeline.addProperty(age55to64, age55to64Value);
-			timeline.addProperty(age65Plus, age65PlusValue);
+			timeline.addProperty(seasonalAdj, seasonalAdjValue);
+			timeline.addProperty(nonSeasonalAdj, nonSeasonalAdjValue);
 		}
 	}
 
